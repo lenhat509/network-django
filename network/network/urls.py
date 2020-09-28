e3a7826581d31c00c2b37cd5e88fbd316b15198a
lenhat <leconganh8436@gmail.com>
2020-09-27 20:17:59 -0700

    build register route

--- network/network/urls.py
+++ network/network/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+from users import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blogs.urls')),
+    path('user/', include('users.urls'))
 ]
---
+++ network/users/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from . forms import UserRegistrationForm
+from django.contrib import messages
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Registered successfully!')
+            return redirect('home')
+    else:
+        form = UserRegistrationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'users/register.html', context=context)
