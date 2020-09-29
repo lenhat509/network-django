f40f281ff9374b3ba3ec238ba82ab5f99015b6ad
lenhat <leconganh8436@gmail.com>
2020-09-28 18:11:14 -0700

    finish authentication system

--- network/users/urls.py
+++ network/users/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth import views as auth_views
     
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout')
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html', redirect_authenticated_user=True), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('profile/', views.profile, name='profile-update')
 ]
