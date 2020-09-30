9b66dd8d48a573e14150cdb994dd0f581ab56c21
lenhat <leconganh8436@gmail.com>
2020-09-29 23:23:25 -0700

    add post model

--- network/posts/models.py
+++ network/posts/models.py
@@ -1,3 +1,21 @@
+from django.urls import reverse
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='interesting_posts', blank=True)
+    comment_to = models.ForeignKey('self', related_name='comments',blank=True, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        if self.comment_to:
+            return f'{self.author.username} post reply to {self.comment_to}'
+        return f'{self.author.username} post'
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
--- network/posts/urls.py
+++ network/posts/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.PostListView.as_view(), name='home'),
+    path('author/<str:username>/', views.UserPostListView.as_view(), name='user-posts'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', views.CreatePostView.as_view(), name='new-post'),
+    path('post/<int:pk>/update/', views.UpdatePostView.as_view(), name='update-post'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='delete-post')
 ]
