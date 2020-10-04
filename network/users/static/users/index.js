1b9664e752444a25aa748375c5526593e0d918c6
lenhat <leconganh8436@gmail.com>
2020-10-03 23:59:31 -0700

    fix litte bugs

--- network/users/static/users/index.js
+++ network/users/static/users/index.js
@@ -1,46 +1,48 @@
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('.post').forEach( post => {
-        post.querySelector('#toggle-like').onclick =  () => {
+    if(document.querySelectorAll('.post')) {
+        document.querySelectorAll('.post').forEach( post => {
+            post.querySelector('#toggle-like').onclick =  () => {
+                const data = {
+                    method: "POST",
+                    credentials:'same-origin',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-CSRFToken': document.cookie.split(';').find(c => c.startsWith('csrftoken')).split('=')[1]
+                    },
+                    body: JSON.stringify({post_id: post.dataset.id})
+                }
+                fetch('http://127.0.0.1:8000/api/like/', data)
+                .then(res => res.json())
+                .then(data => {
+                    if(data.success) {
+                        post.querySelector('#toggle-like').innerHTML = data.res.liked? 'Liked': 'Like'
+                        post.querySelector('#number-likes').innerHTML = data.res.number_likes
+                    }
+                })
+            }
+    })
+    }
+    if(document.querySelector('#toggle-follow')) {
+        document.querySelector('#toggle-follow').onclick = (e) => {
             const data = {
-                method: "POST",
-                credentials:'same-origin',
+                method : "POST",
+                credentials: 'same-origin',
                 headers: {
                     'Content-Type': 'application/json',
-                    'X-CSRFToken': document.cookie.split(';').find(c => c.startsWith('csrftoken')).split('=')[1]
+                    'X-CSRFToken': document.cookie.split(';').find( c => c.startsWith('csrftoken')).split('=')[1]
                 },
-                body: JSON.stringify({post_id: post.dataset.id})
+                body: JSON.stringify({
+                    username: e.target.dataset.username
+                })
             }
-            fetch('http://127.0.0.1:8000/api/like/', data)
+            fetch('http://127.0.0.1:8000/api/follow/', data)
             .then(res => res.json())
             .then(data => {
                 if(data.success) {
-                    post.querySelector('#toggle-like').innerHTML = data.res.liked? 'Liked': 'Like'
-                    post.querySelector('#number-likes').innerHTML = data.res.number_likes
+                    e.target.innerHTML = data.res.following? 'Unfollow': 'Follow'
+                    document.querySelector('#followers').innerHTML = data.res.followers
                 }
             })
         }
-    })
-
-    document.querySelector('#toggle-follow').onclick = (e) => {
-        const data = {
-            method : "POST",
-            credentials: 'same-origin',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-CSRFToken': document.cookie.split(';').find( c => c.startsWith('csrftoken')).split('=')[1]
-            },
-            body: JSON.stringify({
-                username: e.target.dataset.username
-            })
-        }
-        fetch('http://127.0.0.1:8000/api/follow/', data)
-        .then(res => res.json())
-        .then(data => {
-            if(data.success) {
-                e.target.innerHTML = data.res.following? 'Unfollow': 'Follow'
-                document.querySelector('#followers').innerHTML = data.res.followers
-            }
-        })
     }
-
 })
